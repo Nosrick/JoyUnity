803aa0fc5bdc2159036950d5072134c3563bf297
nosrick <nosrick@gmail.com>
2020-11-18 15:40:50 +0000

    Adds Genders (data-driven)

---
+++ Assets/Code/Entities/Gender/BaseGender.cs
@@ -0,0 +1,33 @@
+﻿namespace JoyLib.Code.Entities.Gender
+{
+    public class BaseGender : IGender
+    {
+        public string Possessive { get; protected set; }
+        public string Personal { get; protected set; }
+        public string Reflexive { get; protected set; }
+
+        public string PossessivePlural { get; protected set; }
+
+        public string ReflexivePlural { get; protected set; }
+        public string Name { get; protected set; }
+
+        public BaseGender()
+        {}
+        
+        public BaseGender(
+            string name,
+            string possessive,
+            string personal,
+            string reflexive,
+            string possessivePlural,
+            string reflexivePlural)
+        {
+            this.Name = name;
+            this.Possessive = possessive;
+            this.PossessivePlural = possessivePlural;
+            this.Personal = personal;
+            this.Reflexive = reflexive;
+            this.ReflexivePlural = reflexivePlural;
+        }
+    }
+}
---
+++ Assets/Code/Entities/Gender/IGender.cs
@@ -0,0 +1,13 @@
+﻿namespace JoyLib.Code.Entities.Gender
+{
+    public interface IGender
+    {
+        string Possessive { get; }
+        string Personal { get; }
+        string Reflexive { get; }
+        string Name { get; }
+
+        string PossessivePlural { get; }
+        string ReflexivePlural { get; }
+    }
+}
