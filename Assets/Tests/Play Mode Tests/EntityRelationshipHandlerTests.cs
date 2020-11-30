3792d4f58f5fca8b78671efdb768e95150a00db2
nosrick <nosrick@gmail.com>
2020-11-30 16:41:29 +0000

    EntityRelationshipHandler tests pass! Implementing new IEntity mocks.

--- Assets/Tests/Play Mode Tests/EntityRelationshipHandlerTests.cs
+++ Assets/Tests/Play Mode Tests/EntityRelationshipHandlerTests.cs
@@ -31,106 +31,24 @@ namespace Tests
     {
         private ScriptingEngine scriptingEngine;
 
-        private GameObject inventoryManager;
-
         private IEntityRelationshipHandler target;
-
-        private IEntitySkillHandler SkillHandler;
-        private IEntityTemplateHandler TemplateHandler;
-        private INeedHandler NeedHandler;
-        private ILiveEntityHandler EntityHandler;
-
-        private WorldInstance world;
         
-        private Entity left;
-        private Entity right;
+        private IEntity left;
+        private IEntity right;
         
         [SetUp]
         public void SetUp()
         {
-            inventoryManager = new GameObject();
-            inventoryManager.AddComponent<InventoryManager>();
-
             scriptingEngine = new ScriptingEngine();
 
             target = new EntityRelationshipHandler();
-            NeedHandler = new NeedHandler();
-            SkillHandler = new EntitySkillHandler(NeedHandler);
-            TemplateHandler = new EntityTemplateHandler(SkillHandler);
-            EntityHandler = new LiveEntityHandler();
         }
 
         [SetUp]
         public void SetUpEntities()
         {
-            EntityTemplate random = TemplateHandler.GetRandom();
-            IGrowingValue level = new ConcreteGrowingValue(
-                "level",
-                1,
-                GlobalConstants.DEFAULT_SUCCESS_THRESHOLD,
-                0f,
-                GlobalConstants.DEFAULT_SUCCESS_THRESHOLD,
-                new StandardRoller(new RNG()),
-                new NonUniqueDictionary<INeed, float>());
-
-            ICulture culture = Mock.Of<ICulture>( c => c.GetNameForChain(
-                                                           It.IsAny<int>(), 
-                                                           It.IsAny<string>(), 
-                                                           It.IsAny<int>()) == "NAME"
-            && c.NameData == new NameData[] { new NameData("NAME", new []{0, 1}, new []{"all"}, new int[0]) });
-            List<ICulture> cultures = new List<ICulture> {culture};
-
-            IGender gender = Mock.Of<IGender>(g => g.Name == "female");
-            IBioSex sex = Mock.Of<IBioSex>(s => s.Name == "female"
-            && s.CanBirth == true);
-            ISexuality sexuality = Mock.Of<ISexuality>(s => s.WillMateWith(
-                It.IsAny<Entity>(), It.IsAny<Entity>(), It.IsAny<IRelationship[]>()) == true
-                && s.Tags == new List<string>());
-            IRomance romance = Mock.Of<IRomance>(r => r.Compatible(
-                It.IsAny<Entity>(), It.IsAny<Entity>(), It.IsAny<IRelationship[]>()) == true);
-            IJob job = Mock.Of<IJob>();
-            
-            world = new WorldInstance(
-                new WorldTile[0,0], 
-                new string[0], 
-                "TESTING",
-                EntityHandler,
-                new RNG());
-            
-            left = new Entity(
-                random,
-                new BasicValueContainer<INeed>(NeedHandler.GetManyRandomised(NeedHandler.NeedNames)),
-                cultures,
-                level,
-                job,
-                gender,
-                sex,
-                sexuality,
-                romance,
-                Vector2Int.down,
-                new Sprite[0], 
-                world,
-                new StandardDriver());
-
-            right = new Entity(
-                random,
-                new BasicValueContainer<INeed>(NeedHandler.GetManyRandomised(NeedHandler.NeedNames)),
-                cultures,
-                level,
-                job,
-                gender,
-                sex,
-                sexuality,
-                romance,
-                Vector2Int.down,
-                new Sprite[0], 
-                world,
-                new StandardDriver());
-
-            left.PlayerControlled = true;
-            
-            world.AddEntity(left);
-            world.AddEntity(right);
+            left = Mock.Of<IEntity>(entity => entity.GUID == 1);
+            right = Mock.Of<IEntity>(entity => entity.GUID == 2);
         }
 
         [UnityTest]
@@ -167,7 +85,6 @@ namespace Tests
         [TearDown]
         public void TearDown()
         {
-            GameObject.DestroyImmediate(inventoryManager);
         }
     }
 }
