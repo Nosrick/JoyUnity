eb83e1c6c97bf9caa72576ffd86defb27bb1c592
nosrick <nosrick@gmail.com>
2020-12-10 17:54:17 +0000

    Adds Derived Value information classes

---
+++ Assets/Code/Unity/GUI/DerivedValueBarContainer.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace JoyLib.Code.Unity.GUI
+{
+    public class DerivedValueBarContainer : DerivedValueContainer
+    {
+        [SerializeField] protected Image BarImage;
+
+        public override int DecreaseValue(int delta = 1)
+        {
+            if (this.m_Parent is null)
+            {
+                if (this.Value - delta >= this.Minimum)
+                {
+                    this.Value -= delta;
+                }
+                return this.Value;
+            }
+
+            return base.DecreaseValue(delta);
+        }
+
+        public override int IncreaseValue(int delta = 1)
+        {
+            if (this.m_Parent is null)
+            {
+                if (this.Value + delta <= this.Maximum)
+                {
+                    this.Value += delta;
+                }
+
+                return this.Value;
+            }
+            
+            return base.IncreaseValue(delta);
+        }
+
+        protected void SetFillAmount()
+        {
+            this.BarImage.fillAmount = (float)this.Value / (float)this.Maximum;
+        }
+
+        public override int Value
+        {
+            get => base.Value;
+            set
+            {
+                base.Value = value;
+                this.SetFillAmount();
+            }
+        }
+
+        public override int Maximum
+        {
+            get => this.m_Maximum;
+            set
+            {
+                base.Maximum = value;
+                this.SetFillAmount();
+            }
+        }
+    }
+}
---
+++ Assets/Code/Unity/GUI/Main HUD/DerivedValuesInformation.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using DevionGames.UIWidgets;
+using JoyLib.Code.Collections;
+using JoyLib.Code.Entities;
+using JoyLib.Code.Entities.Statistics;
+using UnityEngine;
+
+namespace JoyLib.Code.Unity.GUI
+{
+    public class DerivedValuesInformation : UIWidget
+    {
+        [SerializeField] protected DerivedValueBarContainer DerivedValuePrefab;
+        [SerializeField] protected GameManager GameManager;
+
+        protected List<DerivedValueBarContainer> Items { get; set; }
+        protected IEntity Player { get; set; }
+
+        public void OnEnable()
+        {
+            if (this.GameManager is null)
+            {
+                return;
+            }
+
+            if (this.Items is null)
+            {
+                this.Items = new List<DerivedValueBarContainer>();
+            }
+
+            this.Player = this.GameManager.Player;
+        }
+
+        public void SetUpDerivedValues(BasicValueContainer<IDerivedValue> values)
+        {
+            List<IDerivedValue> valueList = new List<IDerivedValue>(values.Values);
+            
+            if (this.Items.Count < valueList.Count)
+            {
+                for (int i = this.Items.Count; i < valueList.Count; i++)
+                {
+                    DerivedValueBarContainer newItem =
+                        Instantiate(this.DerivedValuePrefab, this.transform).GetComponent<DerivedValueBarContainer>();
+                    newItem.gameObject.SetActive(true);
+                    this.Items.Add(newItem);
+                }
+            }
+            
+            for(int i = 0; i < valueList.Count; i++)
+            {
+                this.Items[i].Name = valueList[i].Name;
+                this.Items[i].DirectValueSet(valueList[i].Value);
+                this.Items[i].Minimum = -valueList[i].Maximum;
+                this.Items[i].Maximum = valueList[i].Maximum;
+            }
+        }
+    }
+}
