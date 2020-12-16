c5dc5005228750d85a90eced5cfee0a1c694d15f
nosrick <nosrick@gmail.com>
2020-12-16 22:07:14 +0000

    Adds trade window scaling (hopefully)

---
+++ Assets/Code/Unity/GUI/ResizeGridLayoutCells.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace JoyLib.Code.Unity.GUI
+{
+    [RequireComponent(typeof(GridLayoutGroup))]
+    public class ResizeGridLayoutCells : MonoBehaviour
+    {
+        protected int Columns { get; set; }
+        protected int Rows { get; set; }
+
+        public void OnEnable()
+        {
+            GridLayoutGroup grid = this.GetComponent<GridLayoutGroup>();
+            RectTransform rectTransform = this.GetComponent<RectTransform>();
+            RectTransform childRect = this.transform.GetChild(0).GetComponent<RectTransform>();
+            float thisAspect = rectTransform.rect.width / rectTransform.rect.height;
+            float childAspect = (grid.spacing.x + grid.cellSize.x) / (grid.spacing.y + grid.cellSize.y);
+            float newSize = 0;
+
+            float columnWidth = 0;
+            float rowHeight = 0;
+            if (grid.constraint == GridLayoutGroup.Constraint.FixedColumnCount)
+            {
+                this.Columns = grid.constraintCount;
+                columnWidth = (rectTransform.rect.width - (grid.spacing.x * this.Columns)) / this.Columns;
+                this.Rows = (this.transform.childCount / this.Columns); 
+                rowHeight = (rectTransform.rect.height - (grid.spacing.y * this.Rows)) / this.Rows;
+                newSize = Math.Min(rowHeight, columnWidth);
+                grid.cellSize = new Vector2(newSize, newSize);
+            }
+            else if (grid.constraint == GridLayoutGroup.Constraint.FixedRowCount)
+            {
+                this.Rows = grid.constraintCount;
+                rowHeight = (rectTransform.rect.height - (grid.spacing.y * this.Rows)) / this.Rows;
+                this.Columns = (this.transform.childCount / this.Rows);
+                columnWidth = (rectTransform.rect.width - (grid.spacing.x * this.Columns)) / this.Columns;
+                newSize = Math.Min(rowHeight, columnWidth);
+                grid.cellSize = new Vector2(newSize, newSize);
+            }
+        }
+    }
+}
