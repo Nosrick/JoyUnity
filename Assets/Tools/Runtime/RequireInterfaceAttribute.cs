9a294245a3567baf020058aa0fb68545570e02e8
nosrick <nosrick@gmail.com>
2021-01-02 23:17:28 +0000

    Adding more tools and moving around the old ones.

---
+++ Assets/Tools/Runtime/RequireInterfaceAttribute.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Tools
+{
+    public class RequireInterfaceAttribute : PropertyAttribute
+    {
+        // Interface type.
+        public System.Type RequiredType { get; protected set; }
+        /// <summary>
+        /// Requiring implementation of the <see cref="T:RequireInterfaceAttribute"/> interface.
+        /// </summary>
+        /// <param name="type">Interface type.</param>
+        public RequireInterfaceAttribute(System.Type type)
+        {
+            this.RequiredType = type;
+        }
+    }
+}
